7055ab6040c5f3056ce5314177e8792b447211d0
kabir19 <kashyap.wadekar321@gmail.com>
2019-10-30 11:01:31 +0530

    Title capitalized

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <Hero>
         <Banner
-          title="luxurious rooms"
+          title="LUXURIOUS ROOMS / luxurious rooms"
           subtitle="deluxe rooms starting at $299"
         >
           <Link to="/rooms" className="btn-primary">
